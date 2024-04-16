9f6c2db5ed72b8803f26ffd43abaaae381641bec
湾仔 <Wendy@shiwanwandembp.home>
2024-04-16 16:45:05 +0100

    hosting changes

--- db/connection.js
+++ db/connection.js
@@ -10,7 +10,7 @@ if (ENV === 'production'){
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
-console.log("Your environment: ", ENV);
+
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
