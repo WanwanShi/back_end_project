b921ef0a3a40fa8cb28bc41088a626603dd69f9a
湾仔 <Wendy@shiwanwandembp.home>
2024-05-03 17:37:55 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const apiRouter = require("./routes/api-router");
-const {handleCustomErrors,handlePsqlErrors,handleServerErrors,} = require('./errors/index');
-
+const {
+	handleCustomErrors,
+	handlePsqlErrors,
+	handleServerErrors,
+} = require("./errors/index");
+app.use(cors());
 app.use(express.json());
-app.use("/api", apiRouter)
-app.get("*",(req,res,next) => {
-    res.status(404).send({ msg: "Route does not exist" })
-})
+app.use("/api", apiRouter);
+app.get("*", (req, res, next) => {
+	res.status(404).send({ msg: "Route does not exist" });
+});
 
 app.use(handleCustomErrors);
 app.use(handlePsqlErrors);
 app.use(handleServerErrors);
 
-
-
-module.exports = app
+module.exports = app;
